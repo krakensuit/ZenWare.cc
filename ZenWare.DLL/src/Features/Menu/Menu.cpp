e2be94a95c69a73497e8a37142a93b81411e2c76
krakensuit <224611871+krakensuit@users.noreply.github.com>
2026-09-15 20:57:49 +0500

    v3.20.0: stage 6 frosted backdrop behind the panel (flag bEnableBlur) + stage 7 polish (version badge pill, grid-aligned scrollbar)

--- ZenWare.DLL/src/Features/Menu/Menu.cpp
+++ ZenWare.DLL/src/Features/Menu/Menu.cpp
@@ -518,7 +518,7 @@ void CFeatures_Menu::Render(){
     const int nThumbH=U::Math.Clamp((int)(nView*flFrac),20,nView);
     const int nMaxS=m_nContentH[nTabC]-nView;
     const int nThumbY=nTop+(nMaxS>0?(m_nScroll[nTabC]*(nView-nThumbH))/nMaxS:0);
-    const int nSX=m_rc.nX+m_rc.nW-7;
+   const int nSX=m_rc.nX+m_rc.nW-Layout::kPadding/2;
     G::Draw.Rect(nSX,nTop,3,nView,Color(255,255,255,12));
     G::Draw.Rect(nSX,nThumbY,3,nThumbH,CLR_ACCENT_SOFT);
    }
@@ -666,7 +666,10 @@ void CFeatures_Menu::DrawPanel(){
  G::Draw.Rect(m_rc.nX,m_rc.nY,2,m_rc.nH,CLR_ACCENT_SOFT);
  G::Draw.Rect(m_rc.nX+2,m_rc.nY,m_rc.nW-2,Layout::kHeaderH,CLR_HEADER);
 	DrawRgbLogo(m_rc.nX+16,m_rc.nY+4);
-	G::Draw.String(EFonts::MENU_BODY,m_rc.nX+m_rc.nW-14-G::Draw.GetTextWidth(EFonts::MENU_BODY,Vars::Menu::kVersion),m_rc.nY+15,CLR_ACCENT,TXT_DEFAULT,"%s",Vars::Menu::kVersion);
+  const int nBadgeW=G::Draw.GetTextWidth(EFonts::MENU_SMALL,Vars::Menu::kVersion)+10;
+  G::Draw.Rect(m_rc.nX+m_rc.nW-14-nBadgeW,m_rc.nY+12,nBadgeW,18,Color(CLR_ACCENT.r(),CLR_ACCENT.g(),CLR_ACCENT.b(),26));
+  G::Draw.OutlinedRect(m_rc.nX+m_rc.nW-14-nBadgeW,m_rc.nY+12,nBadgeW,18,Color(CLR_ACCENT.r(),CLR_ACCENT.g(),CLR_ACCENT.b(),90));
+  G::Draw.String(EFonts::MENU_SMALL,m_rc.nX+m_rc.nW-9-(nBadgeW/2),m_rc.nY+15,CLR_ACCENT,TXT_CENTERXY,"%s",Vars::Menu::kVersion);
  {
   int hlw = (int)((m_rc.nW - 2) * m_flAnim);
   int hlx = m_rc.nX + 1 + ((m_rc.nW - 2) - hlw) / 2;
--- ZenWare.Loader/resource.h
+++ ZenWare.Loader/resource.h
@@ -7,9 +7,9 @@
 // THE single place holding the release version. Change it here and it is picked up
 // automatically by the window title, the splash and the exe VERSIONINFO.
 #define ZENWARE_VER_MAJOR 3
-#define ZENWARE_VER_MINOR 19
+#define ZENWARE_VER_MINOR 20
 #define ZENWARE_VER_PATCH 0
 
-#define ZENWARE_VER_STR "3.19.0"
+#define ZENWARE_VER_STR "3.20.0"
 
 
